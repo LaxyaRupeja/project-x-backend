e9bf6cbfbd1829098d87803970498baf6dca6479
apoorv0220 <apoorv0220@gmail.com>
2024-04-27 00:41:28 +0530

    Form implemented

---
+++ models/form.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+  index: { type: Number, required: true},
+  ans_type: {
+    type: String,
+    required: true,
+    enum: ["mcq", "dropdown", "checkbox", "short_ans", "long_ans"],
+  },
+  correct_ans: { type: String },
+  optionsArray: {
+    type: [String],
+    validate: {
+      validator: function (value) {
+        if (
+          ["mcq", "dropdown", "checkbox"].includes(this.ans_type) &&
+          (!Array.isArray(value) || value.length === 0)
+        ) {
+          return false;
+        }
+        return true;
+      },
+      message: (props) =>
+        `optionsArray must have at least one element for 'mcq', 'dropdown', or 'checkbox' types`,
+    },
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const formSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    //   required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    //   required: true,
+    },
+    questions: {
+      type: [questionSchema],
+      validate: {
+        validator: function (arr) {
+          // Check if the questions array has at least one element
+          return arr && arr.length > 0;
+        },
+        message: "At least one question is required",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Form = mongoose.model("Form", formSchema);
+
+export default Form;
