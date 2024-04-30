733b8f3b8057d654dc42d410ed5a8f7231f42702
LaxyaRupeja <altlakshi@gmail.com>
2024-04-30 23:38:54 +0530

    added reset password api's

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ import cors from "cors";
 import router from "./routes/user.route.js";
 import formRouter from "./routes/form.router.js";
 import fileRouter from "./routes/file.router.js";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+dotenv.config();
+
 
 const PORT = 8080;
 const app = express();
 
+
+
 connectDB();
+
+// Initializing transporter so it will be generated only once and should not cause any issue 
+export const transporter = nodemailer.createTransport({
+    service: process.env.NODEMAILER_SERVICE,
+    auth: {
+      user: process.env.NODEMAILER_USER, // your Gmail username
+      pass: process.env.NODEMAILER_PASS // your Gmail password
+    }
+});
 
 app.use(express.json());
 app.use(cors())
---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { transporter } from "../index.js";
+dotenv.config();
+
+async function sendEmail(recipient, subject, message) {
+    try {
+      // Define email options
+      let mailOptions = {
+        from: process.env.NODEMAILER_USER,
+        to: recipient,
+        subject: subject,
+        text: message
+      };
+  
+      // Send email
+      let info = await transporter.sendMail(mailOptions);
+  
+      console.log('Email sent: ' + info.response);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+}
+
+
+export default sendEmail;
